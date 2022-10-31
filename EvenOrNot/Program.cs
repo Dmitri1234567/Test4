73dda23d5dd41a328a50ddb1d216efbe26fd9ba9
Dima <dmitrijskurcenko3@gmail.com>
2022-10-31 10:35:30 +0200

    Test
    test

---
+++ EvenOrNot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.Read());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("It is even");
+            }
+            else
+            {
+                Console.WriteLine("It isn't even");
+            }
+        }
+    }
+}
